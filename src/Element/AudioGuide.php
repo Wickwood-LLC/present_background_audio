404bbb92d78822dca29196bc042473ca594616b8
Junaid PV <junu.pv@gmail.com>
2025-02-19 21:34:30 +0530

    Allow setting config for wavesurfer, wrapp with form-elemment div, hidden field to keep the region data.

--- src/Element/AudioGuide.php
+++ src/Element/AudioGuide.php
@@ -21,9 +21,10 @@ class AudioGuide extends FormElementBase {
         [$class, 'preRender'],
       ],
       '#audio_url' => NULL,
-      '#options' => [],
+      '#configs' => [],
       '#attributes' => [],
       '#theme' => 'pba_audio_guide',
+      '#theme_wrappers' => ['form_element'],
       '#attached' => [
         'library' => ['present_background_audio/audio_guide'],
       ],
@@ -32,22 +33,29 @@ class AudioGuide extends FormElementBase {
 
   public static function preRender($element) {
     
-    
+    $encoded_region_data = json_encode($element['#default_value']);
     $element['audio_track'] = [
       '#type' => 'html_tag',
       '#tag' => 'div',
       '#attributes' => [
-        'id' => 'audio-guide-track',
         'data-audio-guide' => 'true',
         'class' => ['audio-guide-track'],
-        'data-configs' => json_encode([
-          'waveColor' => 'rgb(200, 0, 200)',
-          'progressColor' => 'rgb(100, 0, 100)',
-          'url' => $element['#audio_url'],
-          'minPxPerSec' => 100,
-        ]),
-        'data-regions' => $element['#default_value'],
+        'data-configs' => json_encode(
+          $element['#configs'] + [
+            'waveColor' => 'rgb(200, 0, 200)',
+            'progressColor' => 'rgb(100, 0, 100)',
+            'minPxPerSec' => 100,
+          ] + [
+            'url' => $element['#audio_url'],
+          ]
+        ),
+        'data-regions' => $encoded_region_data,
       ],
+    ];
+
+    $element['form_field'] = [
+      '#type' => 'hidden',
+      '#value' => $encoded_region_data,
     ];
     return $element;
   }
