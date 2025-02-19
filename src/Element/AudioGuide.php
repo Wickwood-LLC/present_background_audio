9cbd1c8c42a80e768784f2e4fc6c0eb7e8cba8b5
Junaid PV <junu.pv@gmail.com>
2025-02-19 12:51:50 +0530

    Make audio guide a form element.

---
+++ src/Element/AudioGuide.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Drupal\present_background_audio\Element;
+
+use Drupal\Core\Render\Attribute\RenderElement;
+use Drupal\Core\Render\Element\FormElementBase;
+
+/**
+ * Provides a render element for an auido guide studio.
+ */
+#[RenderElement('pba_audio_guide')]
+class AudioGuide extends FormElementBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getInfo() {
+    $class = static::class;
+    return [
+      '#pre_render' => [
+        [$class, 'preRender'],
+      ],
+      '#presentation' => NULL,
+      '#options' => [],
+      '#attributes' => [],
+      '#theme' => 'pba_audio_guide',
+      '#attached' => [
+        'library' => ['present_background_audio/audio_guide'],
+      ],
+    ];
+  }
+
+  public static function preRender($element) {
+    $presentation = $element['#presentation'];
+    
+    
+    /** @var \Drupal\present\Plugin\RevealJSPlugin\RevealJSPluginManager */
+    $plugin_manager = \Drupal::service('plugin.manager.revealjs_plugins');
+    /** @var \Drupal\present\Plugin\RevealJSPlugin\ConfigurableRevealJSPluginBase */
+    $background_audio = $plugin_manager->getPlugin('background_audio', $presentation);
+
+    $slides = $presentation->getSlides();
+    $regions = [];
+
+    $start = 0;
+    $slide_number = 1;
+    foreach ($slides as $slide) {
+      $slide_duration = !empty($slide['autoslide']) ? intval($slide['autoslide']) / 1000 : 0;
+      $end = $start + $slide_duration;
+      $regions[] = [
+        'start' => $start,
+        'end' => $end,
+        'content' => 'Slide #' . $slide_number,
+        'drag' => $slide_number === 1 ? false : true,
+        'resize' => true,
+        'type' => 'slide',
+      ];
+
+      $dom = new \DOMDocument();
+      $dom->loadHTML($slide['content']);
+
+      $xpath = new \DOMXPath($dom);
+      // Query elements with the class "fragment"
+      $fragments = $xpath->query('//*[contains(@class, "fragment")]');
+      foreach ($fragments as $fragment_index => $fragment) {
+        /** @var \DOMElement $fragment */
+        $fragment_duration = !empty($fragment->getAttribute('data-autoslide')) ? intval($fragment->getAttribute('data-autoslide')) / 1000 : 0;
+        $start = $end;
+        $end = $start + $fragment_duration;
+        $regions[] = [
+          'start' => $start,
+          'end' => $end,
+          'content' => 'Fragment #' . $fragment_index + 1,
+          'drag' => true,
+          'resize' => true,
+          'type' => 'fragment',
+        ];
+      }
+
+      if ($slide_number != count($slides)) {
+        $start = $end;
+        $end = $start + 0.8;
+        $regions[] = [
+          'start' => $start,
+          'end' => $end,
+          'content' => 'Transition #' . $slide_number,
+          'drag' => true,
+          'resize' => false,
+          'color' => 'rgba(100, 100, 100, 0.5)',
+          'type' => 'transition',
+          'fixed_size' => 0.8,
+          'minLength' => 0.8, // This may not be needed.
+          'maxLength' => 0.8, // This may not be needed.
+        ];
+      }
+      $start = $end;
+      
+      $slide_number++;
+    }
+    
+    $element['audio_track'] = [
+      '#type' => 'html_tag',
+      '#tag' => 'div',
+      '#attributes' => [
+        'id' => 'audio-guide-track',
+        'data-audio-guide' => 'true',
+        'class' => ['audio-guide-track'],
+        'data-configs' => json_encode([
+          'waveColor' => 'rgb(200, 0, 200)',
+          'progressColor' => 'rgb(100, 0, 100)',
+          'url' => $background_audio->getConfiguration()['audio_source'],
+          'minPxPerSec' => 100,
+        ]),
+        'data-regions' => json_encode($regions),
+      ],
+    ];
+    return $element;
+  }
+
+}
--- src/Form/PresentationAudioGuideForm.php
+++ src/Form/PresentationAudioGuideForm.php
@@ -60,87 +60,11 @@ class PresentationAudioGuideForm extends EntityForm {
         '@title' => $presentation->label(),
       ]);
     }   
-
-    /** @var \Drupal\present\Plugin\RevealJSPlugin\ConfigurableRevealJSPluginBase */
-    $background_audio = $this->pluginManager->getPlugin('background_audio', $presentation);
-
-    $slides = $presentation->getSlides();
-    $regions = [];
-
-    $start = 0;
-    $slide_number = 1;
-    foreach ($slides as $slide) {
-      $slide_duration = !empty($slide['autoslide']) ? intval($slide['autoslide']) / 1000 : 0;
-      $end = $start + $slide_duration;
-      $regions[] = [
-        'start' => $start,
-        'end' => $end,
-        'content' => 'Slide #' . $slide_number,
-        'drag' => $slide_number === 1 ? false : true,
-        'resize' => true,
-        'type' => 'slide',
-      ];
-
-      $dom = new \DOMDocument();
-      $dom->loadHTML($slide['content']);
-
-      $xpath = new \DOMXPath($dom);
-      // Query elements with the class "fragment"
-      $fragments = $xpath->query('//*[contains(@class, "fragment")]');
-      foreach ($fragments as $fragment_index => $fragment) {
-        /** @var \DOMElement $fragment */
-        $fragment_duration = !empty($fragment->getAttribute('data-autoslide')) ? intval($fragment->getAttribute('data-autoslide')) / 1000 : 0;
-        $start = $end;
-        $end = $start + $fragment_duration;
-        $regions[] = [
-          'start' => $start,
-          'end' => $end,
-          'content' => 'Fragment #' . $fragment_index + 1,
-          'drag' => true,
-          'resize' => true,
-          'type' => 'fragment',
-        ];
-      }
-
-      if ($slide_number != count($slides)) {
-        $start = $end;
-        $end = $start + 0.8;
-        $regions[] = [
-          'start' => $start,
-          'end' => $end,
-          'content' => 'Transition #' . $slide_number,
-          'drag' => true,
-          'resize' => false,
-          'color' => 'rgba(100, 100, 100, 0.5)',
-          'type' => 'transition',
-          'fixed_size' => 0.8,
-          'minLength' => 0.8, // This may not be needed.
-          'maxLength' => 0.8, // This may not be needed.
-        ];
-      }
-      $start = $end;
-      
-      $slide_number++;
-    }
     
     $form['audio_guide'] = [
-      '#type' => 'html_tag',
-      '#tag' => 'div',
-      '#attributes' => [
-        'id' => 'audio-guide-track',
-        'data-audio-guide' => 'true',
-        'class' => ['audio-guide-track'],
-        'data-configs' => json_encode([
-          'waveColor' => 'rgb(200, 0, 200)',
-          'progressColor' => 'rgb(100, 0, 100)',
-          'url' => $background_audio->getConfiguration()['audio_source'],
-          'minPxPerSec' => 100,
-        ]),
-        'data-regions' => json_encode($regions),
-      ],
+      '#type' => 'pba_audio_guide',
+      '#presentation' => $presentation,
     ];
-
-    $form['#attached']['library'][] = 'present_background_audio/audio_guide';
     return $form;
   }
 
