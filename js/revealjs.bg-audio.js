1f43bf93fc2902759cd063ea3d2156aa0997c665
Junaid PV <junu.pv@gmail.com>
2025-03-04 00:08:45 +0530

    No need to store then current button instead just keep the end slide number for the audio guide.

--- js/revealjs.bg-audio.js
+++ js/revealjs.bg-audio.js
@@ -5,7 +5,7 @@ window.RevealBackgroundAudio = window.RevealBackgroundAudio || {
     deck: null,
     // If not null, it will either an Audio element or WaveSurfer instance.
     current_audio: null,
-    current_button: null,
+    end_slide: null,
     configs_to_control: {autoSlide: 1, controls: false, keyboard: false},
     // If true, the audio will be paused during the transition between slides
     pause_during_transition: false,
@@ -38,14 +38,11 @@ window.RevealBackgroundAudio = window.RevealBackgroundAudio || {
             });
         });
         deck.on('slidechanged', (event) => {
-            if (plugin.current_button) {
-                let end_slide = plugin.current_button.getAttribute('data-bg-audio-end-slide');
-                if (end_slide) {
-                    if ( end_slide == (event.indexh)) {
-                        // Note: end slide is 1 based while indexh is 0 based.
-                        // Previous slide is marked as end slide by the button thus stop auto-slide here.
-                        plugin.stopAudio();
-                    }
+            if (plugin.end_slide) {
+                if ( plugin.end_slide == (event.indexh)) {
+                    // Note: end slide is 1 based while indexh is 0 based.
+                    // Previous slide is marked as end slide by the button thus stop auto-slide here.
+                    plugin.stopAudio();
                 }
             }
             if (plugin.current_audio && plugin.pause_during_transition) {
@@ -106,7 +103,7 @@ window.RevealBackgroundAudio = window.RevealBackgroundAudio || {
         let audio_source;
         let audio_guide;
 
-        plugin.current_button = button;
+        plugin.end_slide = button.getAttribute('data-bg-audio-end-slide');
 
         if (plugin.audio_guide_id) {
             audio_guide = document.getElementById(plugin.audio_guide_id);
